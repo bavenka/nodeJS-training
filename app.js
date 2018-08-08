509c0a05e8c8c2eb9805360dfdaf8d866da55c00
Pavel_Kavalchuk <bavenka@gmail.com>
2018-08-08 14:55:14 +0300

    added swagger

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { serve, setup } from 'swagger-ui-express';
+import swaggerJSON from './src/vendors/swagger.json';
 
 import { UserRoute, ProductRoute, CityRoute } from './src/routes';
 
 import cookieParser from './src/middlewares/parsers/cookieParser';
 import queryParser from './src/middlewares/parsers/queryParser';
 
+
+const swaggerOptions = {
+  explorer : true
+};
 
 const app = express();
 
@@ -14,6 +20,7 @@ app.use(bodyParser.json());
 app.use(cookieParser);
 app.use(queryParser);
 
+app.use('/api-swagger', serve, setup(swaggerJSON, swaggerOptions));
 app.use('/api/users', UserRoute);
 app.use('/api/products', ProductRoute);
 app.use('/api/cities', CityRoute);
